93f6f7f5fdddb3163ced6f5b434c96eaf406e4e6
Drozerah <drozerah@drozerah.user>
2018-05-11 20:08:19 +0200

    Set addEventListener +select keys by attributes action or value

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 var Calculator = (function () {
     var self = {};
 
+    // Variables
+    var init = false,
+        calculator,
+        isBtn;
+
+
     // private method
     function test() {
 
@@ -9,15 +15,34 @@ var Calculator = (function () {
 
     // public method
     self.init = function () {
+        
+        calculator = document.querySelector('.calculator');
 
+        
+        // EventListener
+        calculator.addEventListener('click', function (elm) {
 
+            isBtn = elm.target.classList["0"];
+            // is btn        
+            if (isBtn == "btn") {
+                
+                // is key
+                if (elm.target.attributes[1].name == "data-value") {
+                    
+                    console.log(elm.target);
+                    
+                }
 
+                // is action
+                if (elm.target.attributes[1].name == "data-action") {
+                    
+                    console.log(elm.target);
+                    
+                }
 
+            }
 
-
-
-
-        console.log('go !');
+        })        
     };
 
     return self;
