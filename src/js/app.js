92596e5eb81813013a17a58ede1cc0586c6c0530
Drozerah <drozerah@drozerah.user>
2018-05-13 20:45:15 +0200

    Fixed issue : is clicked element has data attribute

--- src/js/app.js
+++ src/js/app.js
@@ -109,99 +109,104 @@ var Calculator = (function () {
     self.init = function () {
         
 
-
         // EventListener
         calculator.addEventListener('click', function (e) {
             
-            thisBtn = e.target;
-            attributeName = thisBtn.attributes[1].name;
-            thisBtnValue = thisBtn.getAttribute(attributeName);
-            isBtn = thisBtn.classList["0"];
 
-            // is btn        
-            if (isBtn == "btn") {
-
-                // is key btn
-                if (attributeName == "data-btn-value") {
-
-
-                    // key btn is not decimal 
-                    if (thisBtnValue !== ".") {
-
-                        arrayPush(thisBtnValue);
-
-                    } else { // key btn is decimal
-
-                        // if isDecimalFrozen = false is true
-                        // the btn is not frozen
-                        if (!isDecimalFrozen) {
-                            console.log('the decimal btn is not frozen');
+            // element has data attributes 
+            if ((e.target.getAttribute("data-btn-value") || e.target.getAttribute("data-btn-action")) ) {
+                // data attribute exist or is empty
+                
+                thisBtn = e.target;
+                attributeName = thisBtn.attributes[1].name;
+                thisBtnValue = thisBtn.getAttribute(attributeName);
+                isBtn = thisBtn.classList["0"];
+    
+                // is btn        
+                if (isBtn == "btn") {
+    
+                    // is key btn
+                    if (attributeName == "data-btn-value") {
+    
+    
+                        // key btn is not decimal 
+                        if (thisBtnValue !== ".") {
+    
                             arrayPush(thisBtnValue);
-                            // freaze 
-                            isDecimalFrozen = true;
-
-                        } else {
-                            console.log('the decimal btn is frozen');
-
+    
+                        } else { // key btn is decimal
+    
+                            // if isDecimalFrozen = false is true
+                            // the btn is not frozen
+                            if (!isDecimalFrozen) {
+                                console.log('the decimal btn is not frozen');
+                                arrayPush(thisBtnValue);
+                                // freaze 
+                                isDecimalFrozen = true;
+    
+                            } else {
+                                console.log('the decimal btn is frozen');
+    
+                            }
                         }
                     }
-                }
-
-                // is operator btn
-                if (attributeName == "data-btn-action") {
-
-
-                    // operators action
-                    if (thisBtnValue == "+" ||
-                        thisBtnValue == "*" ||
-                        thisBtnValue == "/" ||
-                        thisBtnValue == "-" ) {
-
-                        console.log('** ' + thisBtnValue + ' **');
-                        arrayPush(thisBtnValue);
-
-                    }
-
-                    // calculate action
-                    if (thisBtnValue == "=") {
-
+    
+                    // is operator btn
+                    if (attributeName == "data-btn-action") {
+    
+    
+                        // operators action
+                        if (thisBtnValue == "+" ||
+                            thisBtnValue == "*" ||
+                            thisBtnValue == "/" ||
+                            thisBtnValue == "-" ) {
+    
+                            console.log('** ' + thisBtnValue + ' **');
+                            arrayPush(thisBtnValue);
+    
+                        }
+    
+                        // calculate action
+                        if (thisBtnValue == "=") {
+    
+                            // unfreaze decimal btn
+                            isDecimalFrozen = false;
+                            // https://stackoverflow.com/questions/13077923/how-can-i-convert-a-string-into-a-math-operator-in-javascript
+                            result = calculate(reduce);
+                            
+                            // round result
+                            // 2 decimal round
+                            result = Math.round(result * 100) / 100;
+                            // 3 decimal round
+                            //result = Math.round(result * 1000) / 1000;
+                            // 4 decimal round
+                            //result = Math.round(result * 10000) / 10000;
+                            
+                            display.textContent = result;
+                            subDisplay.textContent += thisBtnValue + result;
+    
+                            console.log('arrayNumReducer',arrayNumReducer(array));
+    
+                        }
+    
+                        // clear action
+                        if (thisBtnValue == "clear") {
+                            console.log('** clear **');
+                            subDisplay.textContent = "0";
+                            display.textContent = "0";
+                            array = [];
+                            init = false;
+                            console.log('clear init =', init);
+                        }
+    
                         // unfreaze decimal btn
                         isDecimalFrozen = false;
-                        // https://stackoverflow.com/questions/13077923/how-can-i-convert-a-string-into-a-math-operator-in-javascript
-                        result = calculate(reduce);
-                        
-                        // round result
-                        // 2 decimal round
-                        result = Math.round(result * 100) / 100;
-                        // 3 decimal round
-                        //result = Math.round(result * 1000) / 1000;
-                        // 4 decimal round
-                        //result = Math.round(result * 10000) / 10000;
-                        
-                        display.textContent = result;
-                        subDisplay.textContent += thisBtnValue + result;
-
-                        console.log('arrayNumReducer',arrayNumReducer(array));
-
+    
                     }
-
-                    // clear action
-                    if (thisBtnValue == "clear") {
-                        console.log('** clear **');
-                        subDisplay.textContent = "0";
-                        display.textContent = "0";
-                        array = [];
-                        init = false;
-                        console.log('clear init =', init);
-                    }
-
-                    // unfreaze decimal btn
-                    isDecimalFrozen = false;
-
+    
                 }
 
             }
-
         })
     };
 
