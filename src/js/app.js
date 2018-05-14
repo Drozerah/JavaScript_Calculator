88aadf15c5e2cd9ea74dbaeb5313f9b2211b9e0d
Drozerah <drozerah@drozerah.user>
2018-05-14 13:45:16 +0200

    Add function getResult

--- src/js/app.js
+++ src/js/app.js
@@ -17,10 +17,9 @@ var Calculator = (function () {
         result;
 
     // fix onfocus bug
-    calculator.onfocus = function() {
+    calculator.onfocus = function () {
         this.blur();
-    };    
-        
+    };
 
     // private method
     function arrayPush(str) {
@@ -73,69 +72,117 @@ var Calculator = (function () {
 
     }
 
-    function calculate(fn) {
-        console.log('array',array);
-        
-        return new Function('return ' + fn)();
-    }
-
-    function arrayNumReducer(array){
+    function arrayNumReducer(array) {
 
         var accumulator = [],
             output = [];
-        
+
         for (var i = 0; i < array.length; i++) {
-        
-        if (!(array[i] == '+' || array[i] == '*' || array[i] == '/' || array[i] == '-')) {
-            
-            accumulator.push(array[i]);        
-            
-        } else {
-    
-            output.push(Number(accumulator.join('')));
-            output.push(array[i]);
-            accumulator = [];
-    
-        }    
-        
+
+            if (!(array[i] == '+' || array[i] == '*' || array[i] == '/' || array[i] == '-')) {
+
+                accumulator.push(array[i]);
+
+            } else {
+
+                output.push(Number(accumulator.join('')));
+                output.push(array[i]);
+                accumulator = [];
+
+            }
+
         }
         output.push(Number(accumulator.join('')));
-        
+
         return output;
-    
+
+    }
+
+    function getResult(array) {
+
+        // happy path
+
+        var numbers = [];
+        var operators = [];
+
+        for (var i = 0; i < array.length; i++) {
+
+            // type === numbers
+            if (typeof array[i] === 'number') {
+                numbers.push(array[i]);
+            } else { // is a string
+                operators.push(array[i]);
+            }
+
+        }
+
+        var i = 0;
+        do {
+
+            if (operators[i] == "+") {
+                numbers[0] = numbers[0] + numbers[1];
+            }
+            if (operators[i] == "-") {
+                numbers[0] = numbers[0] - numbers[1];
+            }
+            if (operators[i] == "*") {
+                numbers[0] = numbers[0] * numbers[1];
+            }
+            if (operators[i] == "/") {
+                numbers[0] = numbers[0] / numbers[1];
+            }
+
+            numbers.splice(1, 1);
+            i++;
+        }
+        while (i < operators.length);
+
+        result = numbers[0];
+        // round result
+        // 2 decimal round
+        result = Math.round(result * 100) / 100;
+        // 3 decimal round
+        //result = Math.round(result * 1000) / 1000;
+        // 4 decimal round
+        //result = Math.round(result * 10000) / 10000;
+
+        console.log("result=>", result);
+
+        return result;
+
     }
 
     // public method
     self.init = function () {
-        
+
 
         // EventListener
         calculator.addEventListener('click', function (e) {
-            
+
 
             // element has data attributes 
-            if ((e.target.getAttribute("data-btn-value") || e.target.getAttribute("data-btn-action")) ) {
+            if ((e.target.getAttribute("data-btn-value") || e.target.getAttribute("data-btn-action"))) {
                 // data attribute exist or is empty
-                
+
                 thisBtn = e.target;
                 attributeName = thisBtn.attributes[1].name;
                 thisBtnValue = thisBtn.getAttribute(attributeName);
                 isBtn = thisBtn.classList["0"];
-    
+
                 // is btn        
                 if (isBtn == "btn") {
-    
+
                     // is key btn
                     if (attributeName == "data-btn-value") {
-    
-    
+
+
                         // key btn is not decimal 
                         if (thisBtnValue !== ".") {
-    
+
                             arrayPush(thisBtnValue);
-    
+
                         } else { // key btn is decimal
-    
+
                             // if isDecimalFrozen = false is true
                             // the btn is not frozen
                             if (!isDecimalFrozen) {
@@ -143,52 +190,45 @@ var Calculator = (function () {
                                 arrayPush(thisBtnValue);
                                 // freaze 
                                 isDecimalFrozen = true;
-    
+
                             } else {
                                 console.log('the decimal btn is frozen');
-    
+
                             }
                         }
                     }
-    
+
                     // is operator btn
                     if (attributeName == "data-btn-action") {
-    
-    
+
+
                         // operators action
                         if (thisBtnValue == "+" ||
                             thisBtnValue == "*" ||
                             thisBtnValue == "/" ||
-                            thisBtnValue == "-" ) {
-    
+                            thisBtnValue == "-") {
+
                             console.log('** ' + thisBtnValue + ' **');
                             arrayPush(thisBtnValue);
-    
+
                         }
-    
+
                         // calculate action
                         if (thisBtnValue == "=") {
-    
+
                             // unfreaze decimal btn
                             isDecimalFrozen = false;
                             // https://stackoverflow.com/questions/13077923/how-can-i-convert-a-string-into-a-math-operator-in-javascript
-                            result = calculate(reduce);
                             
-                            // round result
-                            // 2 decimal round
-                            result = Math.round(result * 100) / 100;
-                            // 3 decimal round
-                            //result = Math.round(result * 1000) / 1000;
-                            // 4 decimal round
-                            //result = Math.round(result * 10000) / 10000;
-                            
+                            result = getResult(arrayNumReducer(array));
+
                             display.textContent = result;
                             subDisplay.textContent += thisBtnValue + result;
-    
-                            console.log('arrayNumReducer',arrayNumReducer(array));
-    
+
+                            
+
                         }
-    
+
                         // clear action
                         if (thisBtnValue == "clear") {
                             console.log('** clear **');
@@ -198,12 +238,12 @@ var Calculator = (function () {
                             init = false;
                             console.log('clear init =', init);
                         }
-    
+
                         // unfreaze decimal btn
                         isDecimalFrozen = false;
-    
+
                     }
-    
+
                 }
 
             }
